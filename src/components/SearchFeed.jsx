de37e28bce5000437e8d911e1ef00cd1ff7256f3
wit-l <witty.ysl@gmail.com>
2024-10-07 09:43:37 +0800

    Center the video component in Search Feed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -19,12 +19,18 @@ const SearchFeed = () => {
 
   return (
     <Box p={2} minHeight="95vh">
-      <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
-        Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      <Typography
+        variant="h4"
+        fontWeight={900}
+        color="white"
+        mb={3}
+        ml={{ sm: "100px" }}
+      >
+        Search Results for{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
-      <Box display="flex">
-        <Box sx={{ mr: { sm: '100px' } }}/>
-        {<Videos videos={videos} />}
+      <Box p={2} display="flex">
+        <Videos videos={videos} justifyContent="center" />
       </Box>
     </Box>
   );
